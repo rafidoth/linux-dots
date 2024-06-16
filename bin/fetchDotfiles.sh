cb980d59b2f50bd2b260cafcc7ab6b2a91a735e4
raphy <rafiulhasan803@gmail.com>
2024-06-16 17:37:51 +0600

    --

--- bin/fetchDotfiles.sh
+++ bin/fetchDotfiles.sh
@@ -5,7 +5,7 @@ cp -r ~/.config/i3status ~/dotfiles
 cp -r ~/.config/nvim ~/dotfiles
 cp -r ~/.config/polybar ~/dotfiles
 cp -r ~/.config/rofi ~/dotfiles
-cp -r ~/.config/tmux/tmux.conf ~/dotfiles
+cp -r ~/.config/tmux/tmux.conf ~/dotfiles/tmux
 cp -r ~/.config/picom ~/dotfiles
 cp -r ~/bin ~/dotfiles
 cp -r ~/.bashrc ~/dotfiles
