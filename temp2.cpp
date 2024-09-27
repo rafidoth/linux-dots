181ebdfd511f58d72c693714eb6bc9f66cd77afd
rafidoth <rafiulhasan803@gmail.com>
2024-09-27 09:44:08 +0600

    --

---
+++ temp2.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define MOD 1000000007
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define fill(x,y) memset(x,y,sizeof(x));
+
+
+
+
+
+void l0000000ser(){
+
+}
+
+int main(){
+  fastio();
+  
+  int T;
+  cin >>T;
+  while(T--){
+    l0000000ser();
+  }
+}
+
+
+
+
+
+
